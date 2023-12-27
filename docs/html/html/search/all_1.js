f514cf3057c51c240b2c11ec50de67a69836a623
Rodrigo de Sousa <up202205751@up.pt>
2023-12-27 02:26:44 +0000

    doxygen

--- docs/html/html/search/all_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['controller_0',['Controller',['../class_controller.html',1,'']]],
-  ['csvparser_1',['CSVParser',['../class_c_s_v_parser.html',1,'']]]
-];
--- docs/html/html/search/pages_3.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['important_20requirements_20along_20with_20other_20functionalities_20you_20may_20find_20relevant_0',['In order to develop your flight management system, you should meet the following important requirements, along with other functionalities you may find relevant.',['../md_docs_2tasks.html',1,'']]],
-  ['in_20order_20to_20develop_20your_20flight_20management_20system_20you_20should_20meet_20the_20following_20important_20requirements_20along_20with_20other_20functionalities_20you_20may_20find_20relevant_1',['In order to develop your flight management system, you should meet the following important requirements, along with other functionalities you may find relevant.',['../md_docs_2tasks.html',1,'']]]
-];
